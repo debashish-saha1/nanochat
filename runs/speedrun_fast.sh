18b5134347e6ff5ddbdc0d70da03dba00f11bb3a
Debashish Saha <debashish.saha@stonybrook.edu>
2026-01-30 12:33:01 -0500

    Update speedrun_fast.sh

--- runs/speedrun_fast.sh
+++ runs/speedrun_fast.sh
@@ -45,14 +45,14 @@ python -m scripts.tok_eval
 
 
 # base training
-python -m scripts.base_train --depth=4 --max-seq-len=512 --device-batch-size=1 --total-batch-size=1024 --eval-every=50 --eval-tokens=4096 --core-metric-every=50 --core-metric-max-per-task=12 --sample-every=50 --num-iterations=500 --model-tag=$MODEL_TAG
+python -m scripts.base_train --depth=4 --max-seq-len=512 --device-batch-size=1 --total-batch-size=1024 --eval-every=50 --eval-tokens=4096 --core-metric-every=50 --core-metric-max-per-task=12 --sample-every=50 --num-iterations=5000 --model-tag=$MODEL_TAG
 python -m scripts.base_loss --device-batch-size=1 --split-tokens=4096
 python -m scripts.base_eval --max-per-task=16
 
 # midtraining
 echo "Starting midtraining..."
 # increase iterations for better midtraining = 2*base iterations
-python -m scripts.mid_train --max-seq-len=512 --device-batch-size=1 --eval-every=50 --eval-tokens=4096 --total-batch-size=1024 --num-iterations=500 --device-type=cpu
+python -m scripts.mid_train --max-seq-len=512 --device-batch-size=1 --eval-every=50 --eval-tokens=4096 --total-batch-size=1024 --num-iterations=5000 --device-type=cpu
 
 # eval results will be terrible, this is just to execute the code paths.
 # note that we lower the execution memory limit to 1MB to avoid warnings on smaller systems
@@ -60,7 +60,7 @@ python -m scripts.chat_eval --source=mid --max-new-tokens=128 --max-problems=20
 
 # SFT
 echo "Starting supervised finetuning..."
-python -m scripts.chat_sft --device-batch-size=1 --target-examples-per-step=4 --num-iterations=500 --eval-steps=4 --eval-metrics-max-problems=16 --device-type=cpu 
+python -m scripts.chat_sft --device-batch-size=1 --target-examples-per-step=4 --num-iterations=5000 --eval-steps=4 --eval-metrics-max-problems=16 --device-type=cpu 
 
 echo "..........sft training complete. Starting sft evaluation..."
 python -m scripts.chat_eval -i sft --max-new-tokens=128 --max-problems=16
